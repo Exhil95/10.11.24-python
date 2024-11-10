24abf475a743bc4a0667557cc8f40033efa10cc1
Exhil95 <d.kuczka@gmail.com>
2024-11-10 11:23:01 +0100

    "Added memoization to fibonacci function and updated print statements"

--- rekurencja.py
+++ rekurencja.py
@@ -1,16 +1,22 @@
 import time
-def fibonacci(n):
+
+def fibonacci(n, memo={}):
     if n < 0:
         raise ValueError("Wartość musi być nieujemna")
     elif n == 0:
         return 0
     elif n == 1:
         return 1
+    elif n in memo:
+        return memo[n]
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        result = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+        memo[n] = result
+        return result
+
 start = time.time()
 n = 100 
-print(f"Numer wartość ciągu fibonnaciego dla wartości {n} jest {fibonacci(n)}")
+print(f"Liczba {n} w ciągu Fibonacciego to {fibonacci(n)}")
 koniec = time.time()
 
-print(koniec - start)
+print(f"Czas obliczeń: {koniec - start} sekund")
