ca6195118a7785f560b594686b545738b28ed1be
Exhil95 <d.kuczka@gmail.com>
2024-11-10 11:26:11 +0100

    złe

--- rekurencja.py
+++ rekurencja.py
@@ -1,22 +1,28 @@
 import time
+from functools import lru_cache
 
-def fibonacci(n, memo={}):
+@lru_cache(maxsize=None)
+def fibonacci(n):
+    if not isinstance(n, int):
+        raise TypeError("Wartość musi być liczbą całkowitą")
     if n < 0:
         raise ValueError("Wartość musi być nieujemna")
     elif n == 0:
         return 0
     elif n == 1:
         return 1
-    elif n in memo:
-        return memo[n]
     else:
-        result = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
-        memo[n] = result
-        return result
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-start = time.time()
-n = 100 
-print(f"Liczba {n} w ciągu Fibonacciego to {fibonacci(n)}")
-koniec = time.time()
+def main():
+    start = time.time()
+    n = 200
+    try:
+        print(f"Liczba {n} w ciągu Fibonacciego to {fibonacci(n)}")
+    except (ValueError, TypeError) as e:
+        print(f"Błąd: {e}")
+    end = time.time()
+    print(f"Czas obliczeń: {end - start} sekund")
 
-print(f"Czas obliczeń: {koniec - start} sekund")
+if __name__ == "__main__":
+    main()
